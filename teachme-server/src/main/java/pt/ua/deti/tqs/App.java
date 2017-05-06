6b90d91eab5938fcfe8fcabda91e9c335cac96a3
Tiago Faria <tiagohpf@bitbucket.com>
2017-05-06 21:04:14 +0100

    Generate Maven Archetype

---
+++ teachme-server/src/main/java/pt/ua/deti/tqs/App.java
@@ -0,0 +1,13 @@
+package pt.ua.deti.tqs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
